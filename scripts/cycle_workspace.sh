3374a7f135e3cd6d022831fc5377cc3012010e79
Cole Chapman <colechapman924@gmail.com>
2025-12-14 15:27:51 -0800

    Updated cycling workspaces script

--- scripts/cycle_workspace.sh
+++ scripts/cycle_workspace.sh
@@ -23,13 +23,17 @@ done
 # Decide what to do depending on if moving to next workspace, or previous workspace
 if [ "$DIRECTION" = "next" ]; then
   # This checks to see if the current workspace is the last workspace in the numberical
-  # workspaces. 
+  # workspaces.
   if [ "$current_workspace" -eq "${NUMERICAL_WORKSPACES[-1]}" ]; then
     # If the current workspace is the last numerical workspace, then
     # only move to next workspace if the current workspace has at least 1 window
+    # If the user tries to move to the right again on an empty workspace, they
+    # will just cycle back to the beginning
     WINDOW_COUNT=$(hyprctl activeworkspace -j | jq -r '.windows')
     if [ "$WINDOW_COUNT" -ne 0 ]; then
       hyprctl dispatch workspace $((current_workspace + 1))
+    else
+      hyprctl dispatch workspace e+1
     fi
   # If the current workspace is not at the end, then cycle as normal
   else
